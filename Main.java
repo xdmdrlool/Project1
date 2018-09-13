c3f434b18134f4c5632acd754640b2f0df6af0a3
Clement77b <43254011+Clement77b@users.noreply.github.com>
2018-09-14 00:15:18 +0200

    test main

---
+++ Main.java
@@ -0,0 +1,15 @@
+package fr.clement.InterfaceGraphique;
+
+import java.util.Timer;
+
+import javax.swing.JFrame;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		Fenetre fen =new Fenetre(60);
+
+	}
+
+}
