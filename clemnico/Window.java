4c332afac69b32e808a366fb743b66aaf4e7888c
xdmdrlool <43252481+xdmdrlool@users.noreply.github.com>
2018-09-20 18:36:22 +0200

    maketoto

---
+++ clemnico/Window.java
@@ -0,0 +1,135 @@
+package clemnico;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class Window extends JFrame {
+
+	////Attributs////
+	public Panel 	panel;
+	private JLabel 	statusBar;
+	private int 	fps;
+	
+	Player player =new Player("Player1", 20, 0.2, 0, 300, false); 
+	Portal portal1 =new Portal(-500,-500,20,100);
+	Portal portal2 =new Portal(-500,-500,20,100);
+	FC fc=new FC();
+	
+	////Constructeur////
+	public Window(int fps) {
+		this.fps=fps;
+		
+		this.setTitle("Ma fenetre");
+		this.setSize(400, 500);
+		this.setLocationRelativeTo(null);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+	    this.setVisible(true);
+		this.setResizable(true);
+		
+		portal1.getForm().setColor(Color.BLUE);
+		portal2.getForm().setColor(Color.ORANGE);
+		
+		initPanel();
+		stepGame(player);
+		
+
+	}
+	
+	////Methodes////
+	private void initPanel(){
+		panel=new Panel();
+		panel.setBackground(Color.WHITE);
+		add(panel,BorderLayout.CENTER);
+		
+		statusBar= new JLabel("default");
+		add(statusBar, BorderLayout.SOUTH);
+	
+		Handlerclass handler =new Handlerclass(panel, statusBar, player, portal1, portal2);
+		panel.addMouseListener(handler);
+		panel.addMouseMotionListener(handler);
+		addKeyListener(handler);
+		
+		
+		ArrayList<Form> array = new ArrayList<Form>();
+		array.add(player.getForm());
+		array.add(portal1.getForm());
+		array.add(portal2.getForm());
+		
+		
+		array.add(player.getHitbox().getForm());
+//		array.add(portal1.getHitbox().getForm());
+		
+		panel.setFormList(array);
+		
+	}
+	
+	private void stepGame(Player player) {
+		
+		Timer chrono =new Timer();
+		int delay=100;
+		int period=1000/this.fps;
+		
+		chrono.schedule(new TimerTask() {
+		int temps=0;
+			@Override
+			public void run() {
+				temps=temps+1;
+				player.step(period);
+				
+				panel.repaint();
+				if (temps%1 ==0) {
+//				System.out.println(portal1.getHitbox().getForm().getAngle());
+				
+					//Obtenir les points A et B des deux portails
+					int xAPortal1=(int) fc.Rect2Array(portal1.getForm())[0].x;
+					int yAPortal1=(int) fc.Rect2Array(portal1.getForm())[0].y;
+					int xBPortal1=(int) fc.Rect2Array(portal1.getForm())[1].x;
+					int yBPortal1=(int) fc.Rect2Array(portal1.getForm())[1].y;
+					int xAPortal2=(int) fc.Rect2Array(portal2.getForm())[0].x;
+					int yAPortal2=(int) fc.Rect2Array(portal2.getForm())[0].y;
+					int xBPortal2=(int) fc.Rect2Array(portal2.getForm())[1].x;
+					int yBPortal2=(int) fc.Rect2Array(portal2.getForm())[1].y;
+					
+						
+					if (player.getHitbox().colision(portal1.getHitbox())) {
+						double distancePlayerPortal1=Math.sqrt((player.getX()-xAPortal1)*(player.getX()-xAPortal1)+(player.getY()-yAPortal1)*(player.getY()-yAPortal1));
+						
+						double distancePlayerPortal2=portal2.getWidth()-distancePlayerPortal1;
+						double[] vectorABPortal2= {(xBPortal2-xAPortal2)*1./portal2.getWidth()*1.,(yBPortal2-yAPortal2)*1./portal2.getWidth()*1.};
+						
+						player.moveIn((int) (xAPortal2+vectorABPortal2[0]*distancePlayerPortal2),(int) (yAPortal2+vectorABPortal2[1]*distancePlayerPortal2));
+					}
+					else if (player.getHitbox().colision(portal2.getHitbox())){
+						double distancePlayerPortal2=Math.sqrt((player.getX()-xAPortal2)*(player.getX()-xAPortal2)+(player.getY()-yAPortal2)*(player.getY()-yAPortal2));
+						
+						double distancePlayerPortal1=portal1.getWidth()-distancePlayerPortal2;
+						double[] vectorABPortal1= {(xBPortal1-xAPortal1)*1./portal1.getWidth()*1.,(yBPortal1-yAPortal1)*1./portal1.getWidth()*1.};
+						player.moveIn((int) (xAPortal1+vectorABPortal1[0]*distancePlayerPortal1),(int) (yAPortal1+vectorABPortal1[1]*distancePlayerPortal1));
+					}
+				}
+				
+			}
+				
+			}, delay, period);
+	}
+	
+	////////////////////////////////
+	/////// GETTER AND SETTER //////
+	////////////////////////////////
+	
+	public int getFps() {
+		return fps;
+	}
+	public void setFps(int fps) {
+		this.fps = fps;
+	}
+}
---
+++ clemnico20_09_18/src/clemnico/FormCircle.java
@@ -0,0 +1,139 @@
+package clemnico;
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+public class FormCircle extends Form {
+
+	private String type;
+	private Color color;
+	private int x=100;
+	private int y=100;
+	private int rayon=20;
+
+
+	public FormCircle(Color color, int x, int y,int rayon) {
+		super(color);
+		this.type="CIRCLE";
+		this.setColor(color);
+		this.setX(x);
+		this.setY(y);
+		this.setRayon(rayon);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void draw(Graphics2D graphic) {
+
+		graphic.setColor(this.color);
+		graphic.fillOval(x-rayon, y-rayon, 2*rayon, 2*rayon);
+	}
+	
+	
+	
+	
+	////////////////////////////////
+	/////// GETTER AND SETTER //////
+	////////////////////////////////
+	
+	public String getType() {
+		return type;
+	}
+
+
+	public Color getColor() {
+		return color;
+	}
+
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public int getRayon() {
+		return rayon;
+	}
+
+
+	public void setRayon(int rayon) {
+		this.rayon = rayon;
+	}
+
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+
+
+	@Override
+	public double getAngle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public void setAngle(double angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setWidth(int width) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public void setHeight(int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+}
