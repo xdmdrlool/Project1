d5ef899d48e5a39783835582b68d97196836a27e
xdmdrlool <43252481+xdmdrlool@users.noreply.github.com>
2018-11-07 19:37:19 +0100

    toyuytu

---
+++ clemnico/src/clemnico/EnemyFollow.java
@@ -0,0 +1,89 @@
+package clemnico;
+
+import java.util.ArrayList;
+
+public class EnemyFollow extends Enemy {
+	
+	////Attributs////
+	private int vJump=30;
+	
+	//Son
+	Sound sound=new Sound("pouletHit.wav");
+	
+	
+	////Constructeur////
+	public EnemyFollow(int x,int y,int width, int height,String name, boolean fallFromPlatform) {
+		super(name, x,y, width, height);
+		setVxOnGround(5);
+		setVxMax(vxOnGround);
+		setVx(vxOnGround);
+		setAx(1);
+		
+		setvRecoil(10);
+		setTimeEndRecoil(10);
+		
+		setFallFromPlatform(false);
+	}
+
+
+	////Methodes////
+	public void movement(Player player) {
+		//Mouvement vertical du joueur
+		if (inTheAir) {fall();}
+		//S'il se fait toucher
+		if (isRecoil()) {
+			setX(x+getvRecoil());
+			setTimeRecoil(getTimeRecoil() + 1);
+		}
+		//Sinon mouvement normal
+		else if (!inTheAir){
+			setVx((int)(vx+Math.signum(player.getX()-x)*ax));
+			setX(x+vx);
+			if (vx==0) {
+				setVy(-vJump);
+				setY(y - 1);
+			}
+		}
+		else {
+			setVx((int)(vx+Math.signum(player.getX()-x)*airControl));
+			setX(x+vx);
+		}
+		
+		//Temps de recul
+		if (timeRecoil==timeEndRecoil) {
+			setRecoil(false);
+		}
+	}
+	
+	public void touched(int vxProjectile, int vyProjectile) {
+		sound.play();
+		setRecoil(true);
+		setTimeRecoil(0);
+		setvRecoil((int) Math.signum(vxProjectile)*Math.abs(getvRecoil()));
+	}
+	
+	public void step(Window window,Portal portal1, Portal portal2,ArrayList<Obstacle> obstacles,ArrayList<Entity> entities, Player player) {
+		
+		movement(player);
+		fc.portalInteractionRect(this, portal1, portal2);	// Gestion portails teleportations
+		fc.obstacleInteractionEnemy(this, obstacles);		// Gestion obstacle
+		getCurrentAnimation().update();	
+		chooseAnimation();
+		
+	}
+
+	
+	////////////////////////////////
+	/////// GETTER AND SETTER //////
+	////////////////////////////////
+	
+	public int getvJump() {
+		return vJump;
+	}
+
+
+	public void setvJump(int vJump) {
+		this.vJump = vJump;
+	}
+
+}
---
+++ clemnico/src/clemnico/EnemyJump.java
@@ -0,0 +1,78 @@
+package clemnico;
+
+import java.util.ArrayList;
+
+public class EnemyJump extends Enemy{
+	
+	////Attributs////
+	private int vJump=30;
+	
+	//Son
+	Sound sound=new Sound("pouletHit.wav");
+
+	////Constructeur////
+	public EnemyJump(int x,int y,int width, int height,String name, boolean fallFromPlatform) {
+		super(name, x,y, width, height);
+		
+		setVxOnGround(5);
+		setVxMax(vxOnGround);
+		setVx(vxOnGround);
+		setAx(1);
+		
+		setvRecoil(10);
+		setTimeEndRecoil(10);
+		
+		setFallFromPlatform(true);
+	}
+
+	////Méthodes////
+	public void touched(int vxProjectile, int vyProjectile) {
+		sound.play();
+		setRecoil(true);
+		setTimeRecoil(0);
+		setvRecoil((int) Math.signum(vxProjectile)*Math.abs(getvRecoil()));
+		
+	}
+	
+	public void movement() {
+		//Mouvement vertical du joueur
+		if (inTheAir) {fall();}
+		else {setVy(-vJump);setY(y-10);}
+				
+		if (recoil) {
+			setX(x+getvRecoil());
+			setTimeRecoil(getTimeRecoil() + 1);
+		}
+		else{
+			setVx((int)(vx+Math.signum(vx)*ax));
+			setX(x+vx);
+		}
+		if (getTimeRecoil()==30) {
+			setRecoil(false);
+		}
+		
+	}
+
+	public void step(Window window,Portal portal1, Portal portal2,ArrayList<Obstacle> obstacles,ArrayList<Entity> entities, Player player) {
+		
+		movement();
+			
+		fc.portalInteractionRect(this, portal1, portal2);	// Gestion portails teleportations
+		fc.obstacleInteractionEnemy(this, obstacles);		// Gestion obstacle
+		getCurrentAnimation().update();	
+		chooseAnimation();
+	}
+	
+	////////////////////////////////
+	/////// GETTER AND SETTER //////
+	////////////////////////////////
+
+	public int getvJump() {
+		return vJump;
+	}
+
+	public void setvJump(int vJump) {
+		this.vJump = vJump;
+	}
+
+}
